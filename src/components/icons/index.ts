3f8ea79c0b8e3615caf22339fbbb7297d734d0d7
Chencho34 <armandocrescencio343@gmail.com>
2026-08-03 19:56:51 -0600

    update hero, and business-lines section

---
+++ src/components/icons/index.ts
@@ -0,0 +1,9 @@
+import BusinessLine1 from './BusinessLine_1.astro';
+import BusinessLine2 from './BusinessLine_2.astro';
+import BusinessLine3 from './BusinnesLine_3.astro';
+
+export {
+  BusinessLine1,
+  BusinessLine2,
+  BusinessLine3
+}
---
+++ src/constants/index.ts
@@ -0,0 +1,52 @@
+
+import { BusinessLine1, BusinessLine2, BusinessLine3 } from "../components/icons";
+
+const BusinnesLines = [
+  {
+    img: "images/business-lines-bg-1.webp",
+    title: "Venta de Equipo",
+    description:
+      "Distribuidor oficial DJI Enterprise y Pix4D. Drones industriales, payloads especializados y software de fotogrametría para proyectos de alto nivel.",
+    features: [
+      "DJI Matrice 400/4T/4E",
+      "DJI Dock 3",
+      "Zenmuse H30T, L3, P1",
+      "PIX4Dmapper",
+    ],
+    href: "#",
+    icon: BusinessLine1
+  },
+  {
+    img: "images/business-lines-bg-2.webp",
+    title: "Servicios Profesionales",
+    description:
+      "Ofrecemos servicios profesionales de inspección, mapeo y monitoreo con drones para diversos sectores industriales.",
+    features: [
+      "Inspección de Infraestructura",
+      "Mapeo Topográfico",
+      "Monitoreo Ambiental",
+      "Análisis de Datos Aéreos"
+    ],
+    href: "#",
+    icon: BusinessLine2
+  },
+  {
+    img: "images/business-lines-bg-3.webp",
+    title: "Capacitación y Soporte",
+    description:
+      "Brindamos capacitación especializada y soporte técnico para el uso eficiente de drones y software relacionado.",
+    features: [
+      "Cursos de Pilotaje de Drones",
+      "Capacitación en Fotogrametría",
+      "Soporte Técnico Especializado",
+      "Actualizaciones de Software",
+    ],
+    href: "#",
+    icon: BusinessLine3
+  },
+];
+
+
+export {
+  BusinnesLines
+}
