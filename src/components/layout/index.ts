bf3dfce805655d3b62b1707edbfba88a42bfb1f1
Chencho34 <armandocrescencio343@gmail.com>
2026-08-06 12:04:04 -0600

    update css styles, create Section and Container components

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,9 +1,13 @@
 import Footer from './Footer.astro'
 import Header from './Header.astro'
 import BaseHead from './BaseHead.astro'
+import Section from './Section.astro'
+import Container from './Container.astro'
 
 export {
   Footer,
   Header,
-  BaseHead
+  BaseHead,
+  Section,
+  Container
 }
